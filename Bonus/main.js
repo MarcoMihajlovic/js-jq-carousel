7da3588d5744026d55d999e7cd998aa42c719fef
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-05 16:23:43 +0100

    Bonus Completato

--- Bonus/main.js
+++ Bonus/main.js
@@ -6,23 +6,43 @@ $(document).ready(function() {
     $('.prev').click(function() {
         prevSlide();
     });
+
+    $('.fa-circle').click(function() {
+        $('i').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('id');
+        id = id.split(" ");
+        id = id[1];
+        $('.images img.active').removeClass('active');
+        $('#number' + id).addClass('active');
+    });
 });
 
 var n = $('.images img').length;
 
-/* for (var i = 0; i < n; i++) {
-    $(".images img:nth-child(i+1)").attr('id', 'number' + (i+1));
-} */
+for (var i = 0; i < n; i++) {
+    $(".images img:nth-child(" + (i+1) +")").attr('id', 'number' + (i+1));
+}
 
 //Inserisce tanti pallini quanti sono le immagini
 for (var i = 0; i < n; i++) {
     if(i == 0){
         $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle active first"></i>');
     } else if(i == (n-1)){
-        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle">');
+        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle last">');
     } else {
-        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle last"></i>');
+        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle"></i>');
     }
+}
+
+function clickSlide() {
+    $('i').removeClass('active');
+    $(this).addClass('active');
+    var id = $(this).attr('id');
+    id = id.split(" ");
+    id = id[1];
+    $('.images img.active').removeClass('active');
+    $('#number' + id).addClass('active');
 }
 
 function prevSlide() {
