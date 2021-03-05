e6ff166cd52a0c577e8e7e4b23a5449a3b26574f
Marco <marcosinisa.mihajlovic@gmail.com>
2021-03-05 15:00:51 +0100

    Nav
    Creazione procedurale dei pallini del nav

---
+++ Bonus/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $('.next').click(function() {
+        nextSlide();
+    });
+
+    $('.prev').click(function() {
+        prevSlide();
+    });
+});
+
+var n = $('.images img').length;
+
+/* for (var i = 0; i < n; i++) {
+    $(".images img:nth-child(i+1)").attr('id', 'number' + (i+1));
+} */
+
+//Inserisce tanti pallini quanti sono le immagini
+for (var i = 0; i < n; i++) {
+    if(i == 0){
+        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle active first"></i>');
+    } else if(i == (n-1)){
+        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle">');
+    } else {
+        $('.nav').append('<i id="circle ' + (i+1) + '" class="fas fa-circle last"></i>');
+    }
+}
+
+function prevSlide() {
+    var showImage = $('.images img.active');
+    var showDot = $('.nav i.active');
+
+    showImage.removeClass('active');
+    showDot.removeClass('active');
+
+    if (showImage.hasClass('first')) {
+        $('img.last').addClass('active');
+        $('i.last').addClass('active');
+    } else {
+        showImage.prev('img').addClass('active');
+        showDot.prev('i').addClass('active');
+    }
+}
+
+function nextSlide() {
+    var showImage = $('.images img.active');
+    var showDot = $('.nav i.active');
+
+    showImage.removeClass('active');
+    showDot.removeClass('active');
+
+    if (showImage.hasClass('last')) {
+        $('img.first').addClass('active');
+        $('i.first').addClass('active');
+    } else {
+        showImage.next('img').addClass('active');
+        showDot.next('i').addClass('active');
+    }
+}
